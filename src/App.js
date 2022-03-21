7d30a62dfcb9a899566732322dfba6a8a55845a7
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-21 16:48:38 -0500

    creando archivo de estilos para button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
+import Button from './components/Button'
+import Display from './components/Display'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        todo
+        <Display value={23}/>
+        <Button orange>3</Button>
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>7</Button>
       </div>
     </div>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,12 @@
-export default function Button({children}){
+import './button.css'
+export default function Button({children,...props}){
 
+  //darkGray, orange, lightGray 
+  props.orange ? console.log('todo'):console.log('nada')
+  console.log(props)
   return (
     <>
-      <button>{children}</button>
+      <button className="btn orange">{children}</button>
     </>
   )
 }
