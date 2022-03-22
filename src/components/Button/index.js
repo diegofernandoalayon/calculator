96a6370576fc7775d7a1eba59b6a44f1971ee5eb
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-22 17:40:04 -0500

    adaptando la fuente de cada boton

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,10 +3,10 @@ export default function Button({children,handleClick,...props}){
 
   //darkGray, orange, lightGray 
   // props.orange ? console.log('todo'):console.log('nada')
-  
+  const fontS = children.length>1 ? '1.5rem':'2rem'
   return (
     <>
-      <button className="btn orange" onClick={()=> {handleClick(children)}}>{children}</button>
+      <button style={{fontSize:fontS}} className="btn orange" onClick={()=> {handleClick(children)}}>{children}</button>
     </>
   )
 }
--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,8 +1,8 @@
 export default function Display({value}){
-
+ 
   return (
     <div>
-      <div style={{color:'white',fontSize:'30px'}}>{value}</div>
+      <div style={{color:'white',fontSize:'30px'}}>{value||'0'}</div>
     </div>
   )
 }
