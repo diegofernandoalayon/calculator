4d6077bb8d9e183b99f63ce5a248b12ce1d8c0b7
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-23 19:49:33 -0500

    creando archivo solve.js para resolver las operaciones

---
+++ src/logic/solve.js
@@ -0,0 +1,15 @@
+const OP = {
+  '+':(n1, n2)=> n1+n2,
+  '-':(n1, n2)=> n1-n2,
+  '/':(n1, n2)=> n1/n2,
+  '*':(n1, n2)=> n1*n2
+
+}
+const res = 'Syntax Error'
+const solve = (num1, num2, operation) => {
+  return OP[operation] 
+    ? OP[operation](num1, num2)
+    : res
+
+}
+export default solve 
