c989d3071f783ea08024b200949937082409e063
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-23 19:49:01 -0500

    creando archivo solve.js para resolver las operaciones

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,8 +1,9 @@
 export default function Display({value}){
+
  
   return (
     <div>
-      <div style={{color:'white',fontSize:'30px'}}>{value||'0'}</div>
+      <div style={{color:'white',fontSize:'40px'}}>{value||'0'}</div>
     </div>
   )
 }
