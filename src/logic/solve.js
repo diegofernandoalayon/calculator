f8ec5196b20eb281cdebfc363f8590f50eb1b68c
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-24 20:58:42 -0500

    agregando parametros por defecto a SOLVE, agregando validacion para evitar errores de calculo

--- src/logic/solve.js
+++ src/logic/solve.js
@@ -2,11 +2,12 @@ const OP = {
   '+':(n1, n2)=> n1+n2,
   '-':(n1, n2)=> n1-n2,
   '/':(n1, n2)=> n2!==0 ? n1/n2: 'Math Error',
-  '*':(n1, n2)=> n1*n2
+  'X':(n1, n2)=> n1*n2
 
 }
 const res = 'Syntax Error'
-const solve = (num1, num2, operation) => {
+const solve = ({num1 = 0, num2, operation='+'}={}) => {
+  num2 = (operation === '/' || operation === 'X') ? 1 : 0
   return OP[operation] 
     ? OP[operation](num1, num2)
     : res
