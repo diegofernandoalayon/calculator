10723b0e86d7661db9477b915add7a88039f91be
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-31 16:57:28 -0500

    cambiando el estado

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import './App.css'
 import ButtonPanel from './components/ButtonPanel';
 import calculate from './logic/calculate';
 
+const INITIAL_STATE = {
+  total: '',
+  before: null,
+  operation: null
+}
+
 function App() {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState(INITIAL_STATE)
   const handleClick = (event) =>{
-    calculate(setValue,event)
+    setValue(calculate(value,event))
+    
   }
+  console.log(value.total)
   return (
     <div className="App">
       <div className='container'>
-        <Display value={value}/>
+        <Display value={value.total}/>
         <ButtonPanel handleClick={handleClick}/>
       </div>
     </div>
