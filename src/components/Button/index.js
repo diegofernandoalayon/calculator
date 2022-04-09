0a5d9a228494860d1567f2ded43e0836ce3eeb3b
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-08 20:18:37 -0500

    mejorando estilos

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,23 @@
 import './button.css'
 export default function Button({children,handleClick,...props}){
+  let className = 'btn '
+  if(props?.orange){
+    className += 'orange '
+  }else if(props?.lightGray){
+    className += 'lightGray '
+  }else{
+    className += 'darkGray '
+  }
+  if(props.wide){
+    className += 'wide'
+  }
 
   //darkGray, orange, lightGray 
   // props.orange ? console.log('todo'):console.log('nada')
   const fontS = children.length>1 ? '1.5rem':'2rem'
   return (
     <>
-      <button style={{fontSize:fontS}} className="btn orange" onClick={()=> {handleClick(children)}}>{children}</button>
+      <button style={{fontSize:fontS}} className={className} onClick={()=> {handleClick(children)}}>{children}</button>
     </>
   )
 }
--- src/components/ButtonPanel/index.js
+++ src/components/ButtonPanel/index.js
@@ -1,39 +1,39 @@
 import Button from "../Button";
-
+import './buttonPanel.css'
 export default function ButtonPanel({handleClick}){
 
   return(
     <>
       <div className='panel'>
-        <div>
-          <Button handleClick={handleClick}>C</Button>
-          <Button handleClick={handleClick}>+/-</Button>
-          <Button handleClick={handleClick}>%</Button>
-          <Button handleClick={handleClick}>/</Button>
-        </div>
-        <div>
+        
+          <Button handleClick={handleClick} lightGray>C</Button>
+          <Button handleClick={handleClick} lightGray>+/-</Button>
+          <Button handleClick={handleClick} lightGray>%</Button>
+          <Button handleClick={handleClick} orange>/</Button>
+        
+       
           <Button handleClick={handleClick}>7</Button>
           <Button handleClick={handleClick}>8</Button>
           <Button handleClick={handleClick}>9</Button>
-          <Button handleClick={handleClick}>X</Button>
-        </div>
-        <div>
+          <Button handleClick={handleClick} orange>X</Button>
+        
+        
           <Button handleClick={handleClick}>4</Button>
           <Button handleClick={handleClick}>5</Button>
           <Button handleClick={handleClick}>6</Button>
-          <Button handleClick={handleClick}>-</Button>
-        </div>
-        <div>
+          <Button handleClick={handleClick} orange>-</Button>
+        
+        
           <Button handleClick={handleClick}>1</Button>
           <Button handleClick={handleClick}>2</Button>
           <Button handleClick={handleClick}>3</Button>
-          <Button handleClick={handleClick}>+</Button>
-        </div>
-        <div>
-          <Button handleClick={handleClick}>0</Button>
+          <Button handleClick={handleClick} orange>+</Button>
+        
+        
+          <Button handleClick={handleClick} wide>0</Button>
           <Button handleClick={handleClick}>.</Button>
-          <Button handleClick={handleClick}>=</Button>
-        </div>
+          <Button handleClick={handleClick} orange>=</Button>
+        
       </div>
     </>
   )
