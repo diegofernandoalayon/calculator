173d52e6ac5e1810b8c21a38d172a2d7e5311ebb
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-09 17:14:39 -0500

    agregando estilos

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -3,7 +3,7 @@ export default function Display({value}){
  
   return (
     <div>
-      <div style={{color:'white',fontSize:'40px'}}>{value||'0'}</div>
+      <div style={{color:'white',fontSize:'40px', textAlign: 'right', padding: '20px'}}>{value||'0'}</div>
     </div>
   )
 }
