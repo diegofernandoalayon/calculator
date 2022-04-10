7058aef4b78da10610effcac328e758ee6e09130
Diego Fernando Alayon <dfar.41@gmail.com>
2022-04-10 12:50:20 -0500

    agregando estilos

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,9 +1,13 @@
 export default function Display({value}){
 
+  let fontSize = '60px'
+  if(value){
+    fontSize = value.length < 10 ? '60px' : '40px' 
+  }
  
   return (
     <div>
-      <div style={{color:'white',fontSize:'40px', textAlign: 'right', padding: '20px'}}>{value||'0'}</div>
+      <div style={{color:'white',fontSize:fontSize, textAlign: 'right', padding: '20px', height: '15vh', display: 'flex', justifyContent:'flex-end', alignItems: 'center'}}>{value||'0'}</div>
     </div>
   )
 }
