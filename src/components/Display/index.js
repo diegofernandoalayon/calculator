75c2fc5accf55cfcaef91dc2b7dfc80ef2ee64cc
Diego Fernando Alayon <dfar.41@gmail.com>
2022-12-31 21:25:27 -0500

    agregando comas de miles

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -5,10 +5,33 @@ export default function Display({value}){
   if(value){
     fontSize = value.length < 10 ? '60px' : '40px' 
   }
+
+  const DisplayValue = (value) => {
+    if(value.includes('.')) return value
+    if(value.length > 3){
+      const newValue = value.split('')
+                             .reverse()
+                             .map((n,i) => i%3=== 0 ? `${n},`:n)
+                             .reverse()
+                             .join('')
+                             
+      return newValue.substr(0, newValue.length-1)
+    }
+    return value
+  }
  
   return (
     <div>
-      <div className='display' style={{fontSize:fontSize}}>{value||'0'}</div>
+      <div 
+        className='display' 
+        style={{fontSize:fontSize}}>
+          {
+            value 
+              ? 
+              DisplayValue(value)
+              : '0'
+          }
+      </div>
     </div>
   )
 }
