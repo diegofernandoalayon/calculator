8942777aeb334128cdb13315e6760bc0621b1337
Diego Fernando Alayon <dfar.41@gmail.com>
2023-01-26 12:04:36 -0500

    creando component FullScreen componente icon FullScreen

---
+++ src/components/Icons/FullScreen.js
@@ -0,0 +1,21 @@
+const FullScreen = (props) => (
+  <svg 
+    height={21} 
+    width={21} 
+    viewBox="0 0 21 21"
+    xmlns="http://www.w3.org/2000/svg" 
+    {...props}
+    >
+    <g
+      fill="none"
+      fillRule="evenodd"
+      stroke={props.stroke || "currentColor"}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18.5 7.5V2.522l-5.5.014M18.5 2.522l-6 5.907M13 18.521l5.5.002-.013-5.5M18.5 18.429l-6-5.907M2.5 7.5v-5H8M8.5 8.429l-6-5.907M8 18.516l-5.5.007V13.5M8.5 12.5l-6 6" />
+    </g>
+  </svg>
+)
+
+export default FullScreen
